1809faa560c0290663544af1398d8b081fe63a23
Meet Rajput <contact.meetrajput@gmail.com>
2023-10-12 16:32:50 +0000

    #22 Restructured the  Terraform Root Module

---
+++ providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  #  backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
+  
+  #  cloud {
+  #   organization = "MeetRajput"
+  #   workspaces {
+  #     name = "terra-house-1"
+  #   }
+  # }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.20.0"
+     }
+   }
+}
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
